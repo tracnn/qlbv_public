6006eb0b713f734ef263e62580421232e6d10e30
Tracnn <tracnn20021979@gmail.com>
2024-08-13 13:44:29 +0700

    20240813

--- app/Console/Commands/XML130Import.php
+++ app/Console/Commands/XML130Import.php
@@ -178,7 +178,7 @@ class XML130Import extends Command
                         $this->qd130XmlService->storeQd130XmlInfomation($ma_lk, $macskcb, 'import', $soluonghoso);
                         $this->qd130XmlService->checkQd130XmlComplete($ma_lk);
                         if (config('qd130xml.export_qd130_xml_enabled')) {
-                            //Kiểm tra nếu là XML thông tuyến và inexportable_tt = true thì không thực hiện exportQd130Xml
+                            //Kiểm tra nếu là XML thông tuyến và exportable_tt = false thì không thực hiện exportQd130Xml
                             if (!($disk === 'xml130tt' && config('qd130xml.exportable_tt') == false)) {
                                 $this->qd130XmlService->exportQd130Xml($ma_lk);
                             }                          
@@ -193,7 +193,6 @@ class XML130Import extends Command
         } catch (Exception $e) {
             \Log::error($e->getMessage());
         }
-        
 
     }
 
