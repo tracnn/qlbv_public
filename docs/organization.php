6da7b2bc058913e9d1558c95975545fd9cd83116
Tracnn <tracnn20021979@gmail.com>
2024-10-03 14:48:00 +0700

    20241003

---
+++ docs/organization.php
@@ -0,0 +1,19 @@
+<?php
+
+return [
+    'correct_facility_code' => ['01013'], //Mã nơi đkbd đúng tuyến
+    'exclude_department' => ['K02'], //Mã khoa không cần kiểm tra chỉ định giường trong Xml7450
+    'BHYT' => [
+        'username' => '',
+        'password' => '',
+        'login_url' => 'https://egw.baohiemxahoi.gov.vn/api/token/take',
+        'check_card_url' => 'https://egw.baohiemxahoi.gov.vn/api/egw/NhanLichSuKCB2018', 
+        'enableCheck' => false,
+        'check_card_url_2024' => 'https://egw.baohiemxahoi.gov.vn/api/egw/KQNhanLichSuKCB2024',
+        'hoTenCb' => '',
+        'cccdCb' => '', 
+        'check_by_user' => true,
+        //https://egw.baohiemxahoi.gov.vn
+        //http://14.160.70.2:1431
+    ],
+];
