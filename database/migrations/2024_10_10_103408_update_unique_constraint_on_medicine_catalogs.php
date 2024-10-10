07c16d140602851f3749e1ef0266e7d422ac5191
Tracnn <tracnn20021979@gmail.com>
2024-10-10 10:48:02 +0700

    20241010

---
+++ database/migrations/2024_10_10_103408_update_unique_constraint_on_medicine_catalogs.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class UpdateUniqueConstraintOnMedicineCatalogs extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('medicine_catalogs', function (Blueprint $table) {
+            // Xóa unique constraint cũ nếu có
+            $table->dropUnique('medicine_catalogs_ma_thuoc_ham_luong_so_dang_ky_tt_thau_unique'); // Thay 'ma_thuoc' bằng tên của unique constraint cũ nếu có
+
+            // Thêm unique constraint mới
+            $table->unique(['ma_thuoc', 'ten_thuoc', 'ham_luong', 'don_gia_bh', 'tt_thau', 'tu_ngay'], 'unique_medicine_catalog');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('medicine_catalogs', function (Blueprint $table) {
+            // Xóa unique constraint mới
+            $table->dropUnique('unique_medicine_catalog');
+
+            // Khôi phục lại unique constraint cũ nếu cần
+            $table->unique('medicine_catalogs_ma_thuoc_ham_luong_so_dang_ky_tt_thau_unique'); // Thay bằng tên và cột của unique constraint cũ
+        });
+    }
+}
