b48587e7de801b2d337af92289a0be77d117ae82
Nguyen Ngoc Trac <tracnn20021979@gmail.com>
2024-10-11 20:50:19 +0700

    20241011

--- resources/views/partials/hein_card_filter.blade.php
+++ resources/views/partials/hein_card_filter.blade.php
@@ -3,8 +3,8 @@
         <label for="hein_card_filter">Thẻ BHYT</label>
         <select id="hein_card_filter" class="form-control select2">
             <option value="">Tất cả</option>
-            <option value="has_hein_card">Có thẻ BHYT</option>
-            <option value="has_hein_cards">Có nhiều thẻ BHYT</option>
+            <option value="has_hein_card">Có thẻ BHYT (một)</option>
+            <option value="has_hein_cards">Có thẻ BHYT (nhiều)</option>
             <option value="no_hein_card">Không có thẻ BHYT</option>
         </select>
     </div> 
