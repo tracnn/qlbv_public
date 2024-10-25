19b2b65a3644f3205026a596be3c74092061c4cd
Tracnn <tracnn20021979@gmail.com>
2024-10-25 15:01:15 +0700

    20241025

--- resources/views/vendor/adminlte/page.blade.php
+++ resources/views/vendor/adminlte/page.blade.php
@@ -141,7 +141,7 @@
             <div class="pull-right hidden-xs">
                 <b>Version</b> 3.0.1
             </div>
-            {!! config('adminlte.footer', 'Copyright &#9400; by <b> <a href="http://facebook.com/trac.nguyenngoc" target="_blank">NGUYỄN NGỌC TRÁC</a></b>') !!}
+            {!! config('adminlte.footer', 'Copyright &#9400; by <b> <a href="http://facebook.com/trac.nguyenngoc" target="_blank">NNT</a></b>') !!}
         </footer>
 
     </div>
