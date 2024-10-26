b612b764ed49880c4401cffa41ad07db4882e47c
Nguyen Ngoc Trac <tracnn20021979@gmail.com>
2024-10-26 12:53:20 +0700

    20241026

--- resources/views/emr-checker/partials/medicine-outpatient-detail.blade.php
+++ resources/views/emr-checker/partials/medicine-outpatient-detail.blade.php
@@ -1,34 +1,48 @@
 <!-- File: resources/views/emr-checker/partials/medicine-outpatient-detail.blade.php -->
 @if($medicine_results->isNotEmpty())
+    <style type="text/css">
+    @media print {
+        .no-print { 
+            display: none !important; 
+        }
+
+        .table-print-section th, .table-print-section td {
+            word-wrap: break-word; /* Đảm bảo chữ được xuống dòng nếu cần */
+            white-space: normal; /* Cho phép xuống dòng */
+        }
+    }
+    </style>
+
     <h4>Đơn thuốc ngoại trú
     <button id="print-prescription" class="btn btn-primary" onclick="printPartialContent('.table-print-section');">
         {{ __('In Đơn Thuốc') }}
     </button></h4>
+
     <div class="table table-responsive table-print-section">
         <table class="table table-striped table-bordered table-hover">
             <thead>
                 <tr>
                     <th>{{ __('STT') }}</th>
-                    <th>{{ __('Mã thuốc') }}</th>
+                    <th class="no-print">{{ __('Mã thuốc') }}</th>
                     <th>{{ __('Tên thuốc') }}</th>
                     <th>{{ __('Số lượng') }}</th>
                     <th>{{ __('Đơn vị') }}</th>
                     <th>{{ __('Hàm lượng') }}</th>
                     <th>{{ __('Hướng dẫn') }}</th>
-                    <th>{{ __('Trạng thái') }}</th>
+                    <th class="no-print">{{ __('Trạng thái') }}</th>
                 </tr>
             </thead>
             <tbody>
                 @foreach($medicine_results as $item)
                     <tr>
                         <td>{{ $loop->iteration }}</td>
-                        <td>{{ $item->tdl_service_code }}</td>
+                        <td class="no-print">{{ $item->tdl_service_code }}</td>
                         <td><strong>{{ $item->tdl_service_name }}</strong></td>
                         <td class="text-right"><strong>{{ number_format($item->amount) }}</strong></td>
                         <td>{{ $item->service_unit_name }}</td>
                         <td>{{ $item->tdl_medicine_concentra }}</td>
                         <td>{{ $item->tutorial }}</td>
-                        <td>{{ $item->service_req_stt_name }}</td>
+                        <td class="no-print">{{ $item->service_req_stt_name }}</td>
                     </tr>
                 @endforeach
             </tbody>
@@ -44,7 +58,6 @@
             });
         }
     </script>
-
 @else
     <center>{{ __('insurance.backend.labels.no_information') }}</center>
 @endif
