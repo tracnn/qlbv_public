4d30a56e1c1d76a215a7d2da4c016fe421924989
Tracnn <tracnn20021979@gmail.com>
2024-12-10 14:58:49 +0700

    20241210

--- update.bat
+++ update.bat
@@ -11,7 +11,7 @@ php artisan down
 
 :: Cập nhật mã nguồn từ GitHub
 echo Pulling latest changes from GitHub...
-git checkout -- composer.lock
+git reset --hard
 git pull origin main
 
 :: Chạy các migration (nếu có)
