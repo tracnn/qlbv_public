4d47e1da91ce6ca1a4d884a6450794354c209199
tracnn <tracnn20021979@gmail.com>
2025-03-17 16:05:43 +0700

    20250317

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -137,6 +137,7 @@
 
 <script src="https://code.highcharts.com/highcharts.js"></script>
 <script src="https://code.highcharts.com/highcharts-3d.js"></script>
+<script src="https://code.highcharts.com/modules/exporting.js"></script>
 
 <script type="text/javascript">
     numeral.locale('vi');
