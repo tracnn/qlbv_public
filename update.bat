32f546f6cad5fe66ae02ba96d74190e0b58e4f17
tracnn <tracnn20021979@gmail.com>
2025-03-27 21:29:08 +0700

    20250327

--- update.bat
+++ update.bat
@@ -5,9 +5,33 @@ cd /d "%~dp0"
 :: Đường dẫn đến nssm.exe (giả sử nằm trong thư mục gốc của dự án)
 set NSSM_PATH=%~dp0
 
+:: Đường dẫn đến PHP executable
+set PHP_PATH=php.exe
+
+:: Thư mục gốc chứa ứng dụng Laravel (thư mục hiện tại là thư mục gốc của dự án)
+set LARAVEL_PATH=%~dp0
+
 :: Đưa ứng dụng vào chế độ bảo trì
 echo Putting the application into maintenance mode...
 php artisan down
+
+:: Xóa dịch vụ cho JobQd130Xml
+%NSSM_PATH%\nssm stop "QLBV JobQd130Xml"
+%NSSM_PATH%\nssm remove "QLBV JobQd130Xml" confirm
+
+:: Xóa dịch vụ cho JobKtTheBHYT
+%NSSM_PATH%\nssm stop "QLBV JobKtTheBHYT"
+%NSSM_PATH%\nssm remove "QLBV JobKtTheBHYT" confirm
+
+:: Xóa dịch vụ cho importCatalogBHXH:data
+%NSSM_PATH%\nssm stop "QLBV ImportCatalog"
+%NSSM_PATH%\nssm remove "QLBV ImportCatalog" confirm
+
+:: Xóa dịch vụ cho xml130import:day
+%NSSM_PATH%\nssm stop "QLBV XMLImport"
+%NSSM_PATH%\nssm remove "QLBV XMLImport" confirm
+
+echo Services uninstall completed successfully.
 
 :: Hủy các chỉnh sửa từ local
 echo Clearing changes from Local Git...
@@ -22,12 +46,6 @@ git pull origin main
 :: Chạy các migration (nếu có)
 echo Running migrations...
 php artisan migrate --force
-
-:: Stop từng dịch vụ
-%NSSM_PATH%\nssm stop "QLBV JobQd130Xml"
-%NSSM_PATH%\nssm stop "QLBV JobKtTheBHYT"
-%NSSM_PATH%\nssm stop "QLBV ImportCatalog"
-%NSSM_PATH%\nssm stop "QLBV XMLImport"
 
 :: Dọn dẹp cache
 echo Clearing cache...
@@ -52,11 +70,29 @@ php artisan route:cache
 :: Restart các dịch vụ đã cài đặt
 echo Restarting services...
 
-:: Restart từng dịch vụ
+:: Tạo dịch vụ cho JobQd130Xml
+%NSSM_PATH%\nssm install "QLBV JobQd130Xml" %PHP_PATH% "%LARAVEL_PATH%artisan queue:work --queue=JobQd130Xml"
+%NSSM_PATH%\nssm set "QLBV JobQd130Xml" AppDirectory %LARAVEL_PATH%
+
+:: Tạo dịch vụ cho JobKtTheBHYT
+%NSSM_PATH%\nssm install "QLBV JobKtTheBHYT" %PHP_PATH% "%LARAVEL_PATH%artisan queue:work --queue=JobKtTheBHYT"
+%NSSM_PATH%\nssm set "QLBV JobKtTheBHYT" AppDirectory %LARAVEL_PATH%
+
+:: Tạo dịch vụ cho importCatalogBHXH:data
+%NSSM_PATH%\nssm install "QLBV ImportCatalog" %PHP_PATH% "%LARAVEL_PATH%artisan importCatalogBHXH:data"
+%NSSM_PATH%\nssm set "QLBV ImportCatalog" AppDirectory %LARAVEL_PATH%
+
+:: Tạo dịch vụ cho xml130import:day
+%NSSM_PATH%\nssm install "QLBV XMLImport" %PHP_PATH% "%LARAVEL_PATH%artisan xml130import:day"
+%NSSM_PATH%\nssm set "QLBV XMLImport" AppDirectory %LARAVEL_PATH%
+
+:: Khởi động tất cả các dịch vụ
 %NSSM_PATH%\nssm start "QLBV JobQd130Xml"
 %NSSM_PATH%\nssm start "QLBV JobKtTheBHYT"
 %NSSM_PATH%\nssm start "QLBV ImportCatalog"
 %NSSM_PATH%\nssm start "QLBV XMLImport"
+
+echo Service install completed successfully.
 
 :: Đưa ứng dụng ra khỏi chế độ bảo trì
 echo Bringing the application out of maintenance mode...
