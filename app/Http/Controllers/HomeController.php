123648bcfaeb082953d7cff0091e0af79660fc07
Tracnn <tracnn20021979@gmail.com>
2025-06-16 10:00:16 +0700

    20250615

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -23,12 +23,6 @@ class HomeController extends Controller
      */
     public function index()
     {
-        $ftp = new FtpService();
-        $ftp->connect();
-        $content = $ftp->list();
-        dd($content);
-        $ftp->close();
-
         return view('home');
     }
 
