bfe00ba461fe15532c009e25233808e4e377f0d8
tracnn <tracnn20021979@gmail.com>
2025-06-28 13:45:16 +0700

    20250628

---
+++ app/Models/BhxhEmrPermission.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class BhxhEmrPermission extends Model
+{
+    protected $fillable = ['treatment_code', 'allow_view_at'];
+}
