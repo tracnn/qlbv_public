af29eed7631f2e4da89bea951ae3fd29753e7af4
Tracnn <tracnn20021979@gmail.com>
2025-07-05 17:03:38 +0700

    20250705

--- database/migrations/2025_06_28_201128_add_columns_to_bhxh_emr_permissions_table.php
+++ database/migrations/2025_06_28_201128_add_columns_to_bhxh_emr_permissions_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class AddColumnsToBhxhEmrPermissionsTable1 extends Migration
+class AddColumnsToBhxhEmrPermissionsTable extends Migration
 {
     /**
      * Run the migrations.
--- database/migrations/2025_06_28_213525_add_columns_to_bhxh_emr_permissions_table.php
+++ database/migrations/2025_06_28_213525_add_columns_to_bhxh_emr_permissions_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class AddColumnsToBhxhEmrPermissionsTable2 extends Migration
+class AddColumnsToBhxhEmrPermissionsTable extends Migration
 {
     /**
      * Run the migrations.
